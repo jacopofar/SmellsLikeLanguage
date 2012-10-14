d62d8336d0edba4b8366677859761713ff25a578
jacopofar <jacopo1.farina@gmail.com>
2012-10-14 18:29:27 +0200

    use sbt folder structure, add exception for n<1

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -11,6 +11,7 @@ object Model{
 	val punctuation=".,;:-_!?$%&()[]{}<>=@#*+\\/\"~`\n\r\t";
 }
 class Model(val n:Int,val language:String) {
+  if (n<1) throw new InvalidNGramSizeException("The N-Gram size has to be greater than 0");
 	//val n=ne
 	var ignorePunctuation:Boolean=true
 			private var frequencies=new HashMap[String, Int]
@@ -18,5 +19,7 @@ class Model(val n:Int,val language:String) {
 					 * Increase of 1 the counter of a string presence, creating it if nonexistent
 					 * */
 					def add(s:String)=frequencies.put(s, frequencies.getOrElse(s, 0)+1)
+}
 
+class InvalidNGramSizeException(message:String) extends Throwable{
 }
